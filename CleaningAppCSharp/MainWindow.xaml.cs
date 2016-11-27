7de8290e54d0c173d47b54a1820d09c27a8a5645
Jesus Sanchez <jesusanchez40@gmail.com>
2016-11-26 21:05:56 -0600

    Added Methods class where i have all methods, but now i get some excemption error. Will look later

--- CleaningAppCSharp/MainWindow.xaml.cs
+++ CleaningAppCSharp/MainWindow.xaml.cs
@@ -1,73 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace CleaningAppCSharp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        Methods method = new Methods();
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        private void FeedbackMenu(object sender, RoutedEventArgs e)
         {
-            string command = "mailto:jesus.a.sanchez01@utrgv.edu?subject=Feedback";
-            Process.Start(command);
+            method.FeedbackMethod();
         }
 
-        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        private void AboutMenu(object sender, RoutedEventArgs e)
         {
-            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
-            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
-            string version = fvi.FileVersion;
-            System.Windows.MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Current version: " + version + "\nDo you want to check for a new version?", "Version", System.Windows.MessageBoxButton.YesNo, System.Windows.MessageBoxImage.Asterisk); if (messageBoxResult.ToString() == "Yes") { System.Diagnostics.Process.Start("https://itshaysus.github.io/Cleaningapp/"); }
+            method.AboutMethod();
         }
 
-        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        private void HelpMenu(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                string fileName = "read.pdf";
-                string path = System.IO.Path.Combine(Environment.CurrentDirectory, @"Webcontent\", fileName);
-                System.Diagnostics.Process.Start(path);
-            }           
-            catch (FileNotFoundException)
-            {
-              MessageBox.Show("Documents older than 90 days are not allowed.");
-            }
-            catch (IOException)
-            {
-              MessageBox.Show("Another user is already using this file.");
-            }
-            }
+            method.HelpMethod();  
+        }
 
-            private void LogInButton_Click(object sender, RoutedEventArgs e)
+        private void LoginButton(object sender, RoutedEventArgs e)
         {
+            Window1 win1 = new Window1();
             switch (profiles.SelectedIndex)
             {
                 case 0:
                     MessageBox.Show("Option not available");
                     break;
                 case 1:
-                    Window1 win1 = new Window1();
                     win1.Show();
                     this.Close();
                     break;
--- CleaningAppCSharp/Window1.xaml.cs
+++ CleaningAppCSharp/Window1.xaml.cs
@@ -1,136 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace CleaningAppCSharp
 {
-    /// <summary>
-    /// Interaction logic for Window1.xaml
-    /// </summary>
+
     public partial class Window1 : Window
     {
+        Methods method = new Methods();
+
         public Window1()
         {
             InitializeComponent();
         }
 
-        private void button4_Click(object sender, RoutedEventArgs e)
+        private void Backkupbtn(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                string fileName = "backup.bat";
-                string path = System.IO.Path.Combine(Environment.CurrentDirectory, @"Batchfiles\", fileName);
-                Process.Start(path);
-
-            }
-            catch (FileNotFoundException)
-            {
-                MessageBox.Show("Documents older than 90 days are not allowed.");
-            }
-
+            method.Backupmethod();
         }
 
-        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        private void FeedbackMenu(object sender, RoutedEventArgs e)
         {
-            string command = "mailto:jesus.a.sanchez01@utrgv.edu?subject=Feedback";
-            Process.Start(command);
+            method.FeedbackMethod();
         }
 
-        private void MenuItem_Click_3(object sender, RoutedEventArgs e)
+        private void HelpMenu(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                string fileName = "read.pdf";
-                string path = System.IO.Path.Combine(Environment.CurrentDirectory, @"Webcontent\", fileName);
-                Process.Start(path);
-            }
-            catch (FileNotFoundException)
-            {
-                MessageBox.Show("Documents older than 90 days are not allowed.");
-            }
-            catch (IOException)
-            {
-                MessageBox.Show("Another user is already using this file.");
-            }
+            method.HelpMethod();
         }
 
-        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        private void AboutMenu(object sender, RoutedEventArgs e)
         {
-            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
-            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
-            string version = fvi.FileVersion;
-            MessageBoxResult messageBoxResult = MessageBox.Show("Current version: " + version + "\nDo you want to check for a new version?", "Version", System.Windows.MessageBoxButton.YesNo, System.Windows.MessageBoxImage.Asterisk); if (messageBoxResult.ToString() == "Yes") { System.Diagnostics.Process.Start("https://itshaysus.github.io/Cleaningapp/"); }
+            method.AboutMethod();
         }
 
-        private void button3_Click(object sender, RoutedEventArgs e)
+        private void DiskCleanerButton(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                string command = "/C Cleanmgr /sagerun:1";
-                Process.Start("CMD.exe", command);
-            }
-            catch (IOException)
-            {
-                MessageBox.Show("CCleaner does not exist");
-            }
+            method.DiskCleanerMethod();
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)
+        private void CCleanerMethod(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                string fileName = "ccleaner /auto";
-                string path = System.IO.Path.Combine(Environment.CurrentDirectory, @"Dependencies\", fileName);
-                string command = "/C " + path;
-                Process.Start("CMD.exe", command);
-                MessageBox.Show("CCleaner has started cleaning\nIt will take a few minutes depending on the ammount of junk files on the computer");
-            }
-            catch (IOException)
-            {
-                MessageBox.Show("CCleaner does not exist");
-            }
+            method.CCleanerMethod();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void DefragButton(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                string fileName = "defrag.bat";
-                string path = System.IO.Path.Combine(Environment.CurrentDirectory, @"Webcontent\", fileName);
-                Process.Start(path);
-            }
-            catch (IOException)
-            {
-
-            }
+            method.DefragMethod();
         }
 
-        private void button2_Click(object sender, RoutedEventArgs e)
+        private void DeleteOldFilesButton(object sender, RoutedEventArgs e)
         {
-            
-                 try
-            {
-                string fileName = "Deleteoldfiles.bat";
-                string path = System.IO.Path.Combine(Environment.CurrentDirectory, @"Webcontent\", fileName);
-                Process.Start(path);
-            }
-            catch (IOException)
-            {
+            method.DeleteOldMethods();
+        }
 
-            }
-
+        private void LogOutMenu(object sender, RoutedEventArgs e)
+        {
+            method.LogoutMethod();
         }
     }
 }
